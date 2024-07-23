6789f2d421ec1eca5dd17af1324165e2b335a81e
hnonoyy <hnono0210@gmail.com>
2024-07-23 09:50:43 +0900

    README_수정

---
+++ hello-002/index.js
@@ -0,0 +1,7 @@
+let num1 = 30;
+let num2 = 40;
+let sum = num1 + num2;
+// 문자열 Template, Template Stirng
+// expression interpolation(표현식 삽입법)
+// String.format()
+console.log(`${num1}+${num2} =${sum}`);
